dee28d4cc58310e80a23e5b3bce604683073970e
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-09-16 18:13:21 +0530

    Added grid layout, cta styling

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -75,15 +75,20 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-          // value={withPriority}
-          // onChange={(e) => setWithPriority(e.target.checked)}
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
-          <input type="hidden" name='cart' value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>{isSubmitting ? 'Placing order...' : 'Order now'}</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? 'Placing order...' : 'Order now'}
+          </button>
         </div>
       </Form>
     </div>
@@ -102,7 +107,8 @@ export async function action({ request }: { request: Request }) {
 
   const errors: Record<string, string> = {};
   if (!isValidPhone(order.phone))
-    errors.phone = "Please give us your correct phone number. We might need it to contact you.";
+    errors.phone =
+      'Please give us your correct phone number. We might need it to contact you.';
 
   if (Object.keys(errors).length > 0) return errors;
 
--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Arijit</div>;
+  return <div className="hidden text-sm font-semibold md:block">Arijit</div>;
 }
 
 export default Username;
