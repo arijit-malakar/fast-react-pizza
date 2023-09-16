1acfe03b421b289abed0aa38b1936e74d9f07935
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-09-16 20:03:09 +0530

    Added tailwind config for font, screen height; reusable button, link components

---
+++ src/ui/LinkButton.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+interface LinkButtonProps {
+  to: string;
+}
+
+function LinkButton({ children, to }: PropsWithChildren<LinkButtonProps>) {
+  const navigate = useNavigate();
+  const className = 'text-sm text-blue-500 hover:text-blue-600';
+
+  if (to === '-1') {
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
+  }
+
+  return (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
